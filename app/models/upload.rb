79ad53365afe1a98f63017fe2e91cf6859cb1f83
Igor Putina <iputina@mixbook.com>
2021-02-11 01:04:46 +0200

    Extract meaningful exif values into a method

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,8 +6,25 @@ class Upload < ApplicationRecord
   validates :image, attached: true, content_type: /\Aimage\/.*\z/
 
   def exif
-    return unless image.image?
+    return {} unless image.image?
 
-    @exif ||= Exif::Data.new(self.image.download)
+    {
+      camera: camera,
+      lens: image.metadata[:lens_model],
+      focal_length: image.metadata[:focal_length],
+      exposure_time: image.metadata[:exposure_time],
+      shutter_speed: image.metadata[:shutter_speed_value],
+      aperture: image.metadata[:aperture_value],
+      iso: image.metadata[:iso_speed_ratings]
+    }.compact
+  end
+
+  private
+
+  def camera
+    return nil unless image.metadata[:make]
+    return image.metadata[:make] unless image.metadata[:model]
+
+    "#{image.metadata[:make]} #{image.metadata[:model]}"
   end
 end
