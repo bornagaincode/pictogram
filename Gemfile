9ad19f9d6ccc28216ba2ba5d89c1903aa015844c
Igor Putina <iputina@mixbook.com>
2021-02-11 01:34:11 +0200

    Read exif information from JPEG files (#17)
    * Add `exif` gem
    
    * Implement `exif` attribute on upload model
    
    * Implement exif metadata extraction via `exifr`
    
    * Display exif information

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 gem 'image_processing', '~> 1.2'
 gem 'active_storage_validations', '~> 0.9.2'
+gem 'exifr', '~> 1.3', '>= 1.3.9'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,4 +4,27 @@ class Upload < ApplicationRecord
   has_one_attached :image
 
   validates :image, attached: true, content_type: /\Aimage\/.*\z/
+
+  def exif
+    return {} unless image.image?
+
+    {
+      camera: camera_name,
+      lens: image.metadata[:lens_model],
+      focal_length: image.metadata[:focal_length],
+      exposure_time: image.metadata[:exposure_time],
+      shutter_speed: image.metadata[:shutter_speed_value],
+      aperture: image.metadata[:aperture_value],
+      iso: image.metadata[:iso_speed_ratings]
+    }.compact
+  end
+
+  private
+
+  def camera_name
+    return nil unless image.metadata[:make]
+    return image.metadata[:make] unless image.metadata[:model]
+
+    "#{image.metadata[:make]} #{image.metadata[:model]}"
+  end
 end
